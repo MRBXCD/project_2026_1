34fa01c7cb0fda81f8e7adfcd767f30769b5f604
MRBXCD <bibodong.ac@gmail.com>
2026-04-10 00:50:09 -0400

    Feat: update colab init script with evaluation pipeline

--- shell/rl/colab_init.sh
+++ shell/rl/colab_init.sh
@@ -11,3 +11,6 @@ rm ./data_260311.zip
 # pull sft and grpo adapter
 sh ./shell/rl/rl_deploy.sh
 
+uv run python -m evaluation.pipeline --steps all \
+    --models base,grpo \
+    --skip_sft_for_grpo
